4a5f665fb23f0e1830cdee9a0dd9405a9413f5ea
phillies0997 <60444915+phillies0997@users.noreply.github.com>
2020-04-13 10:04:14 -0400

    Add files via upload

--- Display4.java
+++ Display4.java
@@ -1,14 +1,14 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Display3 extends JFrame {
+public class Display4 extends JFrame {
 
     int WIDTH = 1320;
     int HEIGHT = 1320;
-    int X_POS = (WIDTH / 2) -   100;
-    int Y_POS = (HEIGHT / 2) - 200;
+    int X_POS = (WIDTH / 2) - 250;
+    int Y_POS = (HEIGHT / 2) - 350;
 
-    public Display3(){
+    public Display4(){
         super("Rectangle");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(WIDTH,HEIGHT);
@@ -16,11 +16,11 @@ public class Display3 extends JFrame {
     }
     public void paint(Graphics g){
         super.paint(g);
-        g.drawRect(X_POS, Y_POS, 250, 250);
+        g.drawRect(X_POS, Y_POS, 500,500);
         g.setColor(Color.BLUE);
     }
 
     public static void main(String[] args){
-        Display3 display = new Display3();
+        Display4 display = new Display4();
     }
 }
